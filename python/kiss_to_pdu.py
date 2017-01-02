03b5a5b237e1a10339c33ee00cf92049785a0945
Daniel Estévez <daniel@destevez.net>
2017-01-02 19:10:41 +0100

    Added option in KISS to PDU to handle KISS streams without control byte
    Usually, the first byte of a KISS frame is a control byte. Some applications
    do not follow this convention and there is no control byte. This option allows
    handling this kind of streams.

--- python/kiss_to_pdu.py
+++ python/kiss_to_pdu.py
@@ -33,7 +33,7 @@ class kiss_to_pdu(gr.sync_block):
     """
     docstring for block kiss_to_pdu
     """
-    def __init__(self):
+    def __init__(self, control_byte=True):
         gr.sync_block.__init__(self,
             name="kiss_to_pdu",
             in_sig=[numpy.uint8],
@@ -42,18 +42,18 @@ class kiss_to_pdu(gr.sync_block):
         self.kiss = collections.deque()
         self.pdu = list()
         self.transpose = False
+        self.control_byte = control_byte
 
         self.message_port_register_out(pmt.intern('out'))
 
     def work(self, input_items, output_items):
-#    def handle_msg(self, msg_pmt):
         self.kiss.extend(input_items[0])
         
         while self.kiss:
             c = self.kiss.popleft()
             if c == FEND:
-                if self.pdu and not self.pdu[0] & 0x0f:
-                    msg = array.array('B', self.pdu[1:])
+                if self.pdu and (not self.control_byte or not self.pdu[0] & 0x0f):
+                    msg = array.array('B', self.pdu[1:] if self.control_byte else self.pdu)
                     self.message_port_pub(pmt.intern('out'), pmt.cons(pmt.PMT_NIL, pmt.init_u8vector(len(msg), msg)))
                 self.pdu = list()
             elif self.transpose:
